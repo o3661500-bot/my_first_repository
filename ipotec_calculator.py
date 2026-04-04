19f66255d9bf78789069395f53da1e26dc00ef57
Mikhail <lololoop121@gmail.com>
2026-04-04 17:44:02 -0400

    another commit

--- ipotec_calculator.py
+++ ipotec_calculator.py
@@ -1,5 +1,3 @@
-import keyboard
-
 while True:
     try:
         # Ввод данных для расчета
@@ -15,21 +13,21 @@ while True:
 
         total_loan_term = term_in_years * 12    # Срок кредита в месяцах
 
-        total_rate = (1 + monthly_interest_rate) ** total_loan_term
+        total_rate = (1 + monthly_interest_rate) ** total_loan_term # Общая ставка
 
-        annuity_payment = mortgage_loan_amount * monthly_interest_rate * total_rate / (total_rate - 1)
+        annuity_payment = mortgage_loan_amount * monthly_interest_rate * total_rate / (total_rate - 1)  # Ежемесячный платеж
 
-        overpayment = annuity_payment * total_loan_term - mortgage_loan_amount
+        overpayment = annuity_payment * total_loan_term - mortgage_loan_amount  # Переплата
 
-        # 1. Вычет за покупку (13% от стоимости, но не более чем с 2 млн)
+        # Вычет за покупку (13% от стоимости, но не более чем с 2 млн)
         limit_purchase = 2000000
         tax_refund_purchase = min(real_estate_value, limit_purchase) * 0.13
 
-        # 2. Вычет за проценты по ипотеке (13% от переплаты, но не более чем с 3 млн)
+        # Вычет за проценты по ипотеке (13% от переплаты, но не более чем с 3 млн)
         limit_interests = 3000000
         tax_refund_interests = min(overpayment, limit_interests) * 0.13
 
-        total_refund = tax_refund_purchase + tax_refund_interests
+        total_refund = tax_refund_purchase + tax_refund_interests   # Общая сумма возврата
 
         K = 3.33540331196581
     except ValueError:
@@ -38,16 +36,14 @@ while True:
         print("Ошибка: срок кредита или ставка не могут быть нулевыми.")
 
     print(f"Сумма кредита:{mortgage_loan_amount} ₽")
-    print(f"Ежемесячный платёж:{int(annuity_payment) + 1} ₽")
-    print(f"Переплата по кредиту:{int(overpayment) + 1} ₽")
-    print(f"Общая выплата:{int(mortgage_loan_amount + overpayment) + 1} ₽")
-    print(f"Рекомендуемый доход:{int(annuity_payment * K) + 1}")
+    print(f"Ежемесячный платёж:{(annuity_payment):.0f} ₽")
+    print(f"Переплата по кредиту:{(overpayment):.0f} ₽")
+    print(f"Общая выплата:{(mortgage_loan_amount + overpayment):.0f} ₽")
+    print(f"Рекомендуемый доход:{(annuity_payment * K):.0f}")
     print("НАЛОГОВЫЙ ВЫЧЕТ (НДФЛ):")
-    print(f"За покупку жилья: {tax_refund_purchase:,.0f} ₽")
-    print(f"За уплаченные проценты: {tax_refund_interests:,.0f} ₽")
-    print(f"ИТОГО можно вернуть: {total_refund:,.0f} ₽")
-    print("Нажмите q чтобы завершить ввод")
-
-    if keyboard.is_pressed('q'):
-        print("Программа завершена")
+    print(f"За покупку жилья: {tax_refund_purchase:.0f} ₽")
+    print(f"За уплаченные проценты: {tax_refund_interests:.0f} ₽")
+    print(f"Можно вернуть: {total_refund:.0f} ₽")
+    print("\nНажмите клавишу Enter, чтобы продолжить")
+    if input("Введите q чтобы завершить ввод: ") == "q":
         break
